8ecf3579bf0538c331da7842af8699694e549c37
Justin Ehrlichman <justin.ehrlichman@gmail.com>
2017-05-27 21:28:52 -0400

    Shell script that passes username, password, and host windows inspec command

--- inspec_windows.sh
+++ inspec_windows.sh
@@ -2,5 +2,6 @@
 echo "This will check to make sure all the services are installed and running for this box to be a domain controler as well check for the AD replication status  tool"
 read -p "Enter  username :  " username
 read -s -p  "Enter  password :  " password
-inspec exec jman-windows-spec.rb -t winrm://$username@192.168.1.158 --password=$password --port=5985
+read -p "Enter  host :  " host
+inspec exec jman-windows-spec.rb -t winrm://$username@$host --password=$password --port=5985
 
